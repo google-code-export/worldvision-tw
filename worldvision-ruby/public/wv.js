7672d2662834ebf7fb392c2cccec5db696c3103d
Alive Kuo <alegnadise@gmail.com>
2012-12-22 02:32:52 +0800

    Tab display

--- worldvision-ruby/public/wv.js
+++ worldvision-ruby/public/wv.js
@@ -21,23 +21,36 @@ var WorldVision = {
       $('.pagination:visible li.active').removeClass('active');
       $('.pagination:visible ul li:nth-child('+this.URL.params.em_start+')').addClass('active');
       $('ul.nav.nav-list li.active').removeClass('active');
-      $('ul.nav.nav-list a[href="#tabs-1"]').parent().addClass('active');
-      $('ul.nav.nav-list a[href="#tabs-4"]').parent().addClass('active');
+      if (this.URL.params.type == 'eng') {
+        $('ul.nav.nav-list a[href="#tabs-1"]').parent().addClass('active');
+        $('#tabs-1').siblings().hide();
+        $('#tabs-1').show();
+      } else {
+        $('#tabs-4').siblings().hide();
+        $('#tabs-4').show();
+        $('ul.nav.nav-list a[href="#tabs-4"]').parent().addClass('active');
+      }
     } else if (this.URL.params && this.URL.params.hw_start) {
       $('.pagination:visible li.active').removeClass('active');
       $('.pagination:visible ul li:nth-child('+this.URL.params.hw_start+')').addClass('active');
       $('ul.nav.nav-list li.active').removeClass('active');
       $('ul.nav.nav-list a[href="#tabs-2"]').parent().addClass('active');
+      $('#tabs-2').siblings().hide();
+      $('#tabs-2').show();
     } else if (this.URL.params && this.URL.params.ty_start) {
       $('.pagination:visible li.active').removeClass('active');
       $('.pagination:visible ul li:nth-child('+this.URL.params.ty_start+')').addClass('active');
       $('ul.nav.nav-list li.active').removeClass('active');
       $('ul.nav.nav-list a[href="#tabs-3"]').parent().addClass('active');
+      $('#tabs-3').siblings().hide();
+      $('#tabs-3').show();
     } else if (this.URL.params && this.URL.params.start) {
       $('.pagination:visible li.active').removeClass('active');
       $('.pagination:visible ul li:nth-child('+this.URL.params.start+')').addClass('active');
       $('ul.nav.nav-list li.active').removeClass('active');
       $('ul.nav.nav-list a[href="#tabs-5"]').parent().addClass('active');
+      $('#tabs-5').siblings().hide();
+      $('#tabs-5').show();
     }
 
     if (this.URL.params && this.URL.params.type && this.URL.anchor) {
